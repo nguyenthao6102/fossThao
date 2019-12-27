3d434ca71ff719d1e7e2d74c8c8c9a6e6585a6d7
Nguyen <nguyenthao6102@gmail.com>
2019-12-28 03:54:31 +0700

    commit login logout

--- myblog/blog/urls.py
+++ myblog/blog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
+from django.views.generic import ListView, DetailView
+from .models import Post
 urlpatterns = [
-    path('',views.list, name='blog'),
-    path('<int:id>/', views.post, name='post'),
+    path('', ListView.as_view(
+        queryset = Post.objects.all().order_by("-date"),
+        template_name = 'blog/blog.html',
+        context_object_name = 'Posts',
+        paginate_by =10),
+        name='blog'),
+    path('<int:pk>/', views.Post, name='post'),
 ]
--- myblog/home/views.py
+++ myblog/home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from .forms import RegistrationForm
 # Create your views here.
 
 def index(request):
@@ -8,3 +9,11 @@ def contact(request):
     return render(request, 'pages/contact.html')
 # def error(request):
 #     return render(request, 'pages/error.html')
+def register(request):
+    form = RegistrationForm()
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/')
+    return render(request, 'pages/register.html', {'form': form})
